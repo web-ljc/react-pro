ed4fb3f084fe7b69eb40ce19d39b123dc8720824
web-ljc <aaaa4l@qq.com>
2022-04-08 00:13:21 +0800

    feat: 组件开发&样式模块化

---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import css from './index.module.css'
+
+export default class Hello extends Component{
+  render() {
+    return (
+      <h2 className={css.title}>222</h2>
+    )
+  }
+}
---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import css from './index.module.css'
+
+export default class WelCome extends Component{
+  render() {
+    return (
+      <h2 className={css.title}>WelCome</h2>
+    )
+  }
+}
